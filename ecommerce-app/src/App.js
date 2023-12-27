724fd30ae2a9e4e673f99da34ea394d6b12e238a
Rachel <133939424+rxchell@users.noreply.github.com>
2023-12-27 12:35:18 +0800

    Update App.js

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -20,3 +20,26 @@ function App() {
 }
 
 export default App;
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to E-Commerce Platform</Text>
+      <Text>NAME OF APP</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
