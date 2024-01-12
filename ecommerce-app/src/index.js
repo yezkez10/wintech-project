dd2fcd046da0a2ab9d7dca3cdadf4e95624ce0f5
rxchell <e0969575@u.nus.edu>
2024-01-12 09:30:42 +0800

    gitapply

--- ecommerce-app/src/index.js
+++ ecommerce-app/src/index.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.frontend';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  Carousel,
+  initTE,
+} from "tw-elements";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
- 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+initTE({ Carousel });
+
+const carousel = document.querySelector('.carousel');
+  const slides = document.querySelectorAll('.carousel-item');
+
+  let index = 0;
+
+  function updateCarousel() {
+    carousel.style.transform = `translateX(${-index * 100}%)`;
+  }
+
+  function goToSlide(newIndex) {
+    index = newIndex;
+    updateCarousel();
+  }
+
+  document.querySelectorAll('.btn-circle').forEach((btn, btnIndex) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetSlide = event.target.getAttribute('href').substring(1); 
+      const targetIndex = Array.from(slides).findIndex((slide) => slide.id === targetSlide);
+      goToSlide(targetIndex);
+    });
+  });
