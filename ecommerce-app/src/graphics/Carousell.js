02734a49b45957fd80333e6f3536bf3796ff0a3d
lynnlow175 <lynnlow175@gmail.com>
2024-01-13 20:35:48 +0800

    Add Carousel

--- ecommerce-app/src/graphics/Carousell.js
+++ ecommerce-app/src/graphics/Carousell.js
@@ -1,71 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Carousell() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    let intervalId;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let intervalId;
 
-    useEffect(() => {
-        startAutoSlide();
+  useEffect(() => {
+    console.log("useEffect running");
+    // startAutoSlide();
 
-        return () => {
-            stopAutoSlide();
-        };
-    }, []);
-
-    const slides = [
-        "https://picsum.photos/id/28/367/267",
-        "https://picsum.photos/id/10/367/267",
-        "https://picsum.photos/id/25/367/267",
-        "https://picsum.photos/id/24/367/267",
-    ];
-
-    const showSlide = (index) => {
-        setCurrentIndex(index);
+    return () => {
+      stopAutoSlide();
     };
+  }, []);
 
-    const navigate = (direction) => {
-        const newIndex = (currentIndex + direction + slides.length) % slides.length;
-        showSlide(newIndex);
-    };
+  const slides = [
+    "https://picsum.photos/id/28/367/267",
+    "https://picsum.photos/id/10/367/267",
+    "https://picsum.photos/id/25/367/267",
+    "https://picsum.photos/id/24/367/267",
+  ];
 
-    const autoSlide = () => {
-        navigate(1);
-    };
+  const showSlide = (index) => {
+    console.log(index);
+    setCurrentIndex(index);
+  };
 
-    const startAutoSlide = () => {
-        // Start autoslide and remember the intervalId
-        intervalId = setInterval(autoSlide, 3000); // Slide every 3 seconds
-    };
+  const navigate = (direction) => {
+    const newIndex = (currentIndex + direction + slides.length) % slides.length;
+    showSlide(newIndex);
+  };
 
-    const stopAutoSlide = () => {
-        // Stop autoslide
-        clearInterval(intervalId);
-    };
+  const autoSlide = () => {
+    navigate(1);
+  };
 
-    return (
-        <div className="flex">
-            <div className="carousel-container">
-                <div
-                    className="carousel"
-                    style={{ transform: `translateX(${-currentIndex * 100}%)` }}
-                >
-                    {slides.map((slide, index) => (
-                        <div
-                            key={index}
-                            className="carousel-item"
-                            onClick={() => {
-                                stopAutoSlide(); // Stop autoslide when manually navigating
-                                navigate(index - currentIndex); // Calculate the direction based on the clicked slide
-                                startAutoSlide(); // Restart autoslide after manual navigation
-                            }}
-                        >
-                            <img src={slide} alt={`Slide ${index + 1}`} className="w-full" />
-                        </div>
-                    ))}
-                </div>
+  const startAutoSlide = () => {
+    // Start autoslide and remember the intervalId
+    intervalId = setInterval(autoSlide, 1000); // Slide every 3 seconds
+  };
+
+  const stopAutoSlide = () => {
+    // Stop autoslide
+    clearInterval(intervalId);
+  };
+
+  return (
+    <div className="flex">
+      <div className="carousel-container">
+        <div
+          className="carousel"
+          style={{ transform: `translateX(${-currentIndex * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className="carousel-item"
+              onClick={() => {
+                stopAutoSlide(); // Stop autoslide when manually navigating
+                navigate(index - currentIndex); // Calculate the direction based on the clicked slide
+                startAutoSlide(); // Restart autoslide after manual navigation
+              }}
+            >
+              <img src={slide} alt={`Slide ${index + 1}`} className="w-full" />
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Carousell;
