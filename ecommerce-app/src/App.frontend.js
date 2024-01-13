8e3fc17303a4983a8109b718e944047ba72252bd
lynnlow175 <lynnlow175@gmail.com>
2024-01-13 13:49:35 +0800

    Edit frontend

--- ecommerce-app/src/App.frontend.js
+++ ecommerce-app/src/App.frontend.js
@@ -1,12 +1,12 @@
-import './App.css';
-import ProductDetails from './ProductDetails';
+import "./App.css";
+import "./App.js";
+import ProductDetails from "./ProductDetails";
 
 function App() {
-
   return (
     <div className="App">
       <h2> Welcome to </h2>
-      <MyApp />
+      <App />
       <ProductDetails productId={1} />
       <ProductDetails productId={2} />
       <ProductDetails productId={3} />
