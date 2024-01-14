de01989c015ae81e58f6c184a340814e37322300
rxchell <e0969575@u.nus.edu>
2024-01-14 10:39:44 +0800

    updated the women page

--- ecommerce-app/src/pages/Women.js
+++ ecommerce-app/src/pages/Women.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import NavBar from "../NavBar";
+import Carousel from "../graphics/Carousel.js";
+import slideswomen from "../data/carouselwomen.json";
 
 function Women() {
-    return (
-        <div>Hello, World!</div>
-    );
+  return (
+    <body>
+      <div class="sticky top-0 w-full">
+        <nav class="bg-gray-200 p-2">
+          <ul class="flex  justify-end text-sm">
+            <li class="mr-6">
+              <a class="text-500" href="/Help">
+                Help
+              </a>
+            </li>
+            <li class="mr-6">
+              <a class="text-500" href="/StoreLocator">
+                Store Locator
+              </a>
+            </li>
+            <li class="mr-6">
+              <a class="text-500 font-bold" href="/Language">
+                English
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <NavBar />
+      <Carousel data={slideswomen.slideswomen} />
+
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+    </body>
+  );
 }
 
 export default Women;
